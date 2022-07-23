14f0f96cd4e8c65c2d10f206580eeb024b0c0c49
Ivanek333 <smt22p5681@gmail.com>
2022-07-23 14:29:47 +0300

    Добавьте файлы проекта.

---
+++ OtherClasses.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TreeEvolution
+{
+	public static class d //Debug.Log
+	{
+		public static bool debug;
+		public static void l(object s, bool newline = true)
+		{
+			if (debug)
+				if (newline)
+					Console.WriteLine(s);
+				else
+					Console.Write(s);
+		}
+		public static void c() { Console.Clear(); }
+	}
+
+	public enum CellType
+	{
+		leaf = 0, root, branch, seed, none
+	}
+
+	public struct vec2
+	{
+		public int x, y;
+
+		public vec2(int x, int y)
+		{
+			this.x = x; this.y = y;
+		}
+	}
+
+	public class Tree
+	{
+		public static double MtoEconvert;
+		public static int energyConsume, massToGrow;
+		public static int defaultEnergy, defaultMass;
+		public static int maxLifeTime, maxShadow;
+		public static int level3mass, seedEMscale;
+		
+		public static int reqLeaf, reqRoot, reqBranch, reqSeed;
+
+		public List<Cell> cells;
+		public int energy, mass;
+		public Gen gen;
+		public bool seedState, needToDie, destroy;
+		public int lifeTime;
+
+		public Tree(vec2 pos, bool seed = false, int energy = 0, int mass = 0, List<double> tw = null)
+		{
+			lifeTime = 0;
+			needToDie = false;
+			destroy = false;
+			cells = new List<Cell>();
+			if (seed)
+			{
+				this.seedState = true;
+				cells.Add(new Cell(pos, CellType.seed));
+				Program.world[pos.x][pos.y].block = 1;
+				Program.world[pos.x][pos.y].ctype = CellType.seed;
+				this.energy = energy;
+				this.mass = mass;
+				this.gen = new Gen(tw);
+				this.gen.MutateGen();
+			}
+			else
+			{
+				this.seedState = false;
+				cells.Add(new Cell(pos, CellType.branch));
+				Program.world[pos.x][pos.y].block = 1;
+				Program.world[pos.x][pos.y].ctype = CellType.branch;
+				this.energy = defaultEnergy;
+				this.mass = defaultMass;
+				this.gen = new Gen();
+			}
+		}
+		
+		public bool MatchRequest(int leafc = 0, int rootc = 0, int branchc = 0, int seedc = 0)
+		{
+			int tleaf = 0, troot = 0, tbranch = 0, tseed = 0;
+			foreach (var cell in cells)
+			{
+				switch (cell.ctype)
+				{
+					case CellType.leaf: 
+					    tleaf++; break;
+					case CellType.root: 
+					    troot++; break;
+					case CellType.branch: 
+					    tbranch++; break;
+					case CellType.seed: 
+					    tseed++; break;
+				}
+			}
+			if ((tleaf >= leafc) &&
+			    (troot >= rootc) &&
+			    (tbranch >= branchc) &&
+			    (tseed >= seedc))
+			    return true;
+			return false;
+		}
+
+		public void UpdateTree()
+		{
+			foreach (var cell in cells)
+			{
+				switch (cell.ctype)
+				{
+					case CellType.leaf:
+						energy += Program.world[cell.pos.x][cell.pos.y].energy *
+						          Program.world[cell.pos.x][cell.pos.y].shadow;
+						break;
+					case CellType.branch:
+						//energy += Program.world[cell.pos.x][cell.pos.y].energy / 2;
+						break;
+					case CellType.root:
+						mass += Program.world[cell.pos.x][cell.pos.y].mass;
+						break;
+				}
+				energy -= energyConsume;
+				if (cell.ctype == CellType.seed) energy -= energyConsume * (seedEMscale - 1);
+			}
+			if (mass < 0)
+			{
+				needToDie = true;
+			}
+			else if (energy < 0)
+			{
+				if (mass * MtoEconvert >= -energy)
+				{
+					int t = (int)((-energy) / MtoEconvert);
+					if ((-energy) % MtoEconvert != 0) t++;
+					mass -= t;
+					energy += (int)(t * MtoEconvert);
+				}
+				else
+				{
+					needToDie = true;
+				}
+			}
+		}
+
+		public void GrowTree()
+		{
+			if (seedState)
+			{
+				vec2 topos = new vec2(cells[0].pos.x, cells[0].pos.y - 1);
+				if (topos.y < 0) topos.y = 0;
+				if (Program.world[topos.x][topos.y].block == 1)
+				{
+					needToDie = true;
+					destroy = true;
+				}
+				else if (Program.world[topos.x][topos.y].mass != 0)
+				{
+					if (Program.world[topos.x][topos.y].mass == 1)
+					{
+						cells[0].ctype = CellType.branch;
+						Program.world[cells[0].pos.x][cells[0].pos.y].ctype = CellType.branch;
+						Program.world[cells[0].pos.x][cells[0].pos.y].block = 1;
+						seedState = false;
+					}
+					else
+					{
+						needToDie = true;
+						destroy = true;
+					}
+				}
+				else
+				{
+					Program.world[cells[0].pos.x][cells[0].pos.y].block = 0;
+					Program.world[cells[0].pos.x][cells[0].pos.y].ctype = CellType.none;
+					Program.world[topos.x][topos.y].block = 1;
+					Program.world[topos.x][topos.y].ctype = CellType.seed;
+					cells[0].pos = topos;
+				}
+			}
+			else if (lifeTime > maxLifeTime)
+			{
+				needToDie = true;
+			}
+			else
+			{
+				lifeTime++;
+				int count = cells.Count;
+				d.l(count);
+				for (int l = 0; l < count; l++)
+				{
+					if (!cells[l].growed)
+					{
+						var cell = cells[l];
+						cell.growed = true;
+						List<int> engs = new List<int>();
+						List<int> mss = new List<int>();
+						List<int> wls = new List<int>();
+						List<vec2> neigs = Neigbours(cell.pos, Program.worldSize);
+						foreach (var n in neigs)
+						{
+							engs.Add(Program.world[n.x][n.y].energy);
+							mss.Add(Program.world[n.x][n.y].mass);
+							wls.Add(Program.world[n.x][n.y].block);
+						}
+						List<CellType> t = gen.CalculateOutput(
+							energy, mass,
+							(cell.ctype == CellType.root) ? 1 : 0,
+							(cell.ctype == CellType.branch) ? 1 : 0,
+							engs, mss, wls);
+						for (int i = 0; i < 4; i++)
+						{
+							if ((wls[i] == 0) && (mass > massToGrow) &&
+								(t[i] != CellType.none) &&
+								(Math.Abs(neigs[i].y - cell.pos.y) <= 1) &&
+								Cell.CanGrow(cell.ctype, t[i], neigs[i]) &&
+								!(((i == 0) || (i == 2)) && (neigs[i].y == 3) && (mass < level3mass)))
+							{
+								int minusmass = 0;
+								if (((i == 0) || (i == 2)) && (neigs[i].y == 3))
+								    minusmass = level3mass;
+								else
+							    	minusmass = massToGrow;
+							    if (t[i] == CellType.seed) minusmass *= seedEMscale;
+							    mass -= minusmass;
+								Cell c = new Cell(neigs[i], t[i]);
+								Program.world[c.pos.x][c.pos.y].block = 1;
+								Program.world[c.pos.x][c.pos.y].ctype = c.ctype;
+								cells.Add(c);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public void Die()
+		{
+			foreach (var cell in cells)
+			{
+				Program.world[cell.pos.x][cell.pos.y].block = 0;
+				Program.world[cell.pos.x][cell.pos.y].ctype = CellType.none;
+			    if (!destroy && (cell.ctype == CellType.seed))
+				{
+					Program.trees.Add(new Tree(cell.pos, true, defaultEnergy, defaultMass, gen.w));
+				}
+			}
+		}
+
+		public List<vec2> Neigbours(vec2 pos, vec2 size)
+		{
+			List<vec2> ret = new List<vec2>();
+			ret.Add(new vec2((pos.x - 1 + size.x) % size.x, pos.y));
+			ret.Add(new vec2(pos.x, (pos.y + 1) % size.y));
+			ret.Add(new vec2((pos.x + 1) % size.x, pos.y));
+			ret.Add(new vec2(pos.x, (pos.y - 1 + size.y) % size.y));
+			return ret;
+		}
+	}
+
+	public class Cell
+	{
+		public CellType ctype;
+		public vec2 pos;
+		public bool growed;
+
+		public Cell(vec2 pos, CellType ctype, bool growed = false)
+		{
+			this.pos = pos; this.ctype = ctype; this.growed = growed;
+		}
+
+		public static bool CanGrow(CellType parent, CellType child, vec2 pos)
+		{
+			bool ret = true;
+			switch (parent)
+			{
+				case CellType.seed: 
+				    ret = false; break;
+				case CellType.leaf:
+					ret = false; break;
+				case CellType.root:
+					if (child == CellType.leaf)
+						ret = false;
+					if (child == CellType.seed)
+						ret = false;
+					break;
+				case CellType.branch:
+				    if ((child == CellType.leaf) ||
+				        (child == CellType.seed))
+				        if (Program.world[pos.x][pos.y].mass != 0)
+				            ret = false;
+				    if (child == CellType.root)
+				        if (Program.world[pos.x][pos.y].mass == 0)
+				            ret = false;
+				    break;
+			}
+			return ret;
+		}
+	}
+
+	public class Gen
+	{
+		public static double randWgenAmp;
+		public static double randWeditAmp;
+		public static double randWeditChance;
+		public static bool lowIQmode;
+
+		public static Random rand;
+
+		public List<double> w;
+
+		public Gen()
+		{
+			CreateRandomWeights();
+		}
+
+		public Gen(List<double> tw)
+		{
+			w = new List<double>();
+			tw.ForEach((double v) => w.Add(v));
+		}
+
+		public void CreateRandomWeights()
+		{
+			w = new List<double>();
+			for (int i = 0; i < 16 * 10 + 10 * 20; i++)
+			{
+				w.Add((rand.Next() % (100 * randWgenAmp * 2)) / 100.0 - randWgenAmp);
+			}
+		}
+
+		public void MutateGen()
+		{
+			for (int i = 0; i < w.Count; i++)
+			{
+				double c = (rand.Next() % 10000) / 100.0;
+				if (c <= randWeditChance)
+				{
+					w[i] += (rand.Next() % (100 * randWeditAmp * 2)) / 100.0 - randWeditAmp;
+				}
+			}
+		}
+
+		public List<CellType> CalculateOutput(int treeE, int treeM, int meRoot, int meBranch, List<int> energies, List<int> masses, List<int> walls)
+		{
+			List<double> middle = new List<double>();
+			List<double> output = new List<double>();
+			List<double> input = new List<double>
+			{ treeE, treeM, meRoot, meBranch };
+			for (int i = 0; i < 4; i++)
+			{
+				input.Add(energies[i]);
+				input.Add(masses[i]);
+				input.Add(walls[i]);
+			}
+			for (int i = 0; i < 10; i++)
+			{
+				middle.Add(0);
+				for (int j = 0; j < input.Count; j++)
+				{
+					middle[i] += input[j] * w[i * input.Count + j];
+				}
+			}
+			for (int i = 0; i < 20; i++)
+			{
+				output.Add(0);
+				for (int j = 0; j < 10; j++)
+				{
+					output[i] += middle[j] * w[16 * 10 + i * 10 + j];
+				}
+			}
+			List<CellType> outs = new List<CellType>();
+			List<double> max = new List<double>();
+			for (int i = 0; i < 4; i++)
+			{
+				max.Add(output[i]);
+				outs.Add((CellType)0);
+				for (int j = 1; j < (lowIQmode ? 4 : 5); j++)
+				{
+					if (output[i + j * 4] > max[i])
+					{
+						max[i] = output[i + j * 4];
+						outs[i] = (CellType)j;
+					}
+				}
+			}
+			return outs;
+		}
+	}
+
+	public class FieldCell
+	{
+		public int energy, mass, block, shadow;
+		public CellType ctype;
+
+		public FieldCell(int energy, int mass = 0, int block = 0, CellType ctype = CellType.none, int shadow = 0)
+		{
+			this.energy = energy; this.mass = mass; this.block = block; this.ctype = ctype; this.shadow = shadow;
+		}
+
+		public FieldCell()
+		{
+			energy = 0; mass = 0; block = 0; ctype = CellType.none; shadow = 0;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TreeEvolution
+{
+	public static class Program
+	{
+
+		public static List<List<FieldCell>> world;
+		public static List<Tree> trees;
+		public static vec2 worldSize;
+		public static int alive;
+
+		public static void DrawWorld(int layer)
+		{
+			string s = $"Generation ≈ {alive / Tree.maxLifeTime}";
+			s += $"  Lifetime: {Tree.maxLifeTime}  Iteration: {alive}";
+			for (int j = worldSize.y - 1; j >= 0; j--)
+			{
+				for (int i = 0; i < worldSize.x; i++)
+				{
+					switch (layer)
+					{
+						case 0:
+							s += world[i][j].mass;
+							break;
+						case 1:
+							s += world[i][j].energy;
+							break;
+						case 2:
+							s += world[i][j].block;
+							break;
+						case 3:
+							switch (world[i][j].ctype)
+							{
+								case CellType.none:
+									if (world[i][j].mass > 0)
+										s += "%";
+									//else if (world[i][j].shadow != 0)
+									//    s += world[i][j].shadow; // для просмотра уровня теней
+									else
+										s += " ";
+									break;
+								case CellType.leaf:
+									s += "~"; break;
+								case CellType.branch:
+									s += "@"; break;
+								case CellType.root:
+									s += "#"; break;
+								case CellType.seed:
+									s += "*"; break;
+							}
+							break;
+					}
+				}
+				s += "\n";
+			}
+			Console.Write(s);
+		}
+		
+		public static void UpdateShadows()
+		{
+			for (int i = 0; i < worldSize.x; i++)
+			{
+				int sh = Tree.maxShadow;
+				for (int j = worldSize.y - 1; j >= 0; j--)
+				{
+					world[i][j].shadow = sh;
+					if ((world[i][j].block == 1) && (sh > 0))
+					    sh--;
+				}
+			}
+		}
+
+		public static void Main()
+		{
+			int[] energyVertical = new int[]
+			{ 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+			int[] massVertical = new int[]
+			{ 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+			Tree.MtoEconvert = 1;  //масса превращается в энергию, если она закончилась
+			Tree.defaultEnergy = 20; //энергия семени
+			Tree.defaultMass = 5;    //масса семени
+			Tree.energyConsume = 3;  //расход энергии за один шаг
+			Tree.massToGrow = 1;     //расход массы для роста новых клеток
+			Tree.seedEMscale = 2;    //увеличение расхода для семени
+			Tree.maxLifeTime = 100;  //максимальная продолжительность жизни
+			Tree.maxShadow = 3;      //максимальный уровень теней (сколько преград может преодолеть)
+            Tree.level3mass = 3; // коэфицент увеличение стоимости роста и поддержания жизни на этом уровне
+            
+			Gen.rand = new Random();
+			Gen.randWgenAmp = 2;      //амплитуда при случаном заполнении гена
+			Gen.randWeditAmp = 1;     //амплитуда при мутации
+			Gen.randWeditChance = 5.0;  //шанс мутации в %, можно ставить дробные значения
+			Gen.lowIQmode = false;    //убирает возможность ничего не ставить (хз зачем надо)
+
+			worldSize = new vec2(100, energyVertical.Count());
+
+			int treeSpawnCount = worldSize.x / 15;
+
+			world = new List<List<FieldCell>>();
+			for (int i = 0; i < worldSize.x; i++)
+			{
+				world.Add(new List<FieldCell>());
+				for (int j = 0; j < worldSize.y; j++)
+				{
+					world[i].Add(new FieldCell(energyVertical[j], massVertical[j]));
+				}
+			}
+			DrawWorld(0);
+			Console.WriteLine();
+			DrawWorld(1);
+			Console.WriteLine();
+			DrawWorld(2);
+
+			trees = new List<Tree>();
+			for (int i = 0; i < treeSpawnCount; i++)
+			{
+				vec2 p = new vec2(worldSize.x / treeSpawnCount * i, 3);
+				Tree t = new Tree(p, false);
+				trees.Add(t);
+			}
+			
+			//Требования по кол-ву
+			Tree.reqLeaf = 2;   // листьев
+			Tree.reqRoot = 1;   // корней
+			Tree.reqBranch = 2; // стеблей
+			Tree.reqSeed = 0;   // семян
+			
+			d.debug = false;       //дебаг моде он/офф
+			bool manual = false;   //тыкать на кнопки для следующего шага
+			bool autoreset = true; //если все деревья умерли, автоматически генерировать заново
+			bool matchreq = false; //необоходимо соответствовать требованиям (на определенном шаге)
+			int fastdelay = 1;    //задержка быстрого просмотра
+			int slowdelay = 500;   //задержка медленного просмотра
+			int delay = fastdelay; //текущая задержка
+			alive = 0;             //счетчик шагов
+			int autor = 0;         //счетчик перезапусков (ограничен 50)
+			int skip = 1000;      //сколько шагов скипнуть без отрисовки(супер быстро)
+			int slowtime = 99000;  //время для включения медленной скорости
+			int reqafter = 500;    //после скольких шагов начинать проверять требования
+			while (true)
+			{
+				alive++;
+
+				if (manual && (alive >= skip))
+				{
+					var key = (char)Console.Read();
+					if (key == 'b') break;
+				}
+
+				int tcount = trees.Count;
+
+				d.l("tcount: " + tcount);
+				for (int i = 0; i < tcount; i++)
+				{
+					trees[i].UpdateTree();
+				}
+				for (int i = 0; i < tcount; i++)
+				{
+					d.l("tree: " + i);
+					trees[i].GrowTree();
+					d.l("tree: " + i + " end");
+				}
+				for (int i = 0; i < tcount; i++)
+				{
+					if (trees[i].needToDie)
+					{
+						if ((alive > reqafter) && !trees[i].destroy && matchreq)
+						{
+							if (!trees[i].MatchRequest(Tree.reqLeaf, Tree.reqRoot, Tree.reqBranch, Tree.reqSeed))
+								trees[i].destroy = true;
+						}
+						trees[i].Die();
+						Program.trees.RemoveAt(i);
+						i--;
+						tcount--;
+					}
+				}
+				UpdateShadows();
+				if (alive > skip)
+				{
+					Console.Clear();
+					DrawWorld(3);
+					System.Threading.Thread.Sleep(delay);
+				}
+				else
+				{
+					if (alive % Math.Max((skip / 100), 500) == 0)
+						Console.WriteLine("alive: " + alive + " (" + (alive / (skip / 100)) + "%)");
+				}
+
+				if (trees.Count == 0)
+				{
+					Console.WriteLine("No trees left");
+					if (autoreset)
+					{
+						autor++;
+						if (autor > 50)
+						{
+							Console.WriteLine("Too much resets");
+							break;
+						}
+						else
+						{
+							alive = 0;
+							delay = fastdelay;
+							for (int i = 0; i < treeSpawnCount; i++)
+							{
+								vec2 p = new vec2(worldSize.x / treeSpawnCount * i, 3);
+								Tree t = new Tree(p, false);
+								trees.Add(t);
+							}
+						}
+					}
+					else
+						break;
+				}
+				else if (alive > slowtime)
+				{
+					delay = slowdelay;
+				}
+			}
+			Console.WriteLine("end");
+		}
+	}
+}
